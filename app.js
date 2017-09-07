0359e9ca9e40bfb773257cca59dd0d204b0545ff
Sam Soffar <samsoffar@gmail.com>
2017-09-07 15:59:58 -0400

    Create an Express server

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(request, response){
+
+	response.sendFile(__dirname +'/public/index.html')
+});
+
+app.listen(3000, function (){
+	console.log("listening on port 3000");
+});
