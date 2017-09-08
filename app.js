2184e173944391e2c1f5244ed5e73ce1436eb1f5
Sam Soffar <samsoffar@gmail.com>
2017-09-08 01:17:59 -0400

    use middleware to send a static page

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(request, response){
+// app.get('/', function(request, response){
 
-	response.sendFile(__dirname +'/public/index.html')
-});
+// 	response.sendFile(__dirname +'/public/index.html')
+// });
+app.use(express.static('public'));
 
 app.listen(3000, function (){
-	console.log("listening on port 3000");
+	console.log("listening on port 3000, using static ");
 });
